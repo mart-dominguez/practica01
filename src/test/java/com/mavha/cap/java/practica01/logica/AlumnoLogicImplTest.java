9af1e4dc207e77a2bb07471432013244617fa07c
mart-dominguez <mdomingu@gmail.com>
2017-08-18 12:33:17 -0300

    casos de test

---
+++ src/test/java/com/mavha/cap/java/practica01/logica/AlumnoLogicImplTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mavha.cap.java.practica01.logica;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mdominguez
+ */
+public class AlumnoLogicImplTest {
+    
+    public AlumnoLogicImplTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of costoPromedio method, of class AlumnoLogicImpl.
+     */
+    @Test
+    public void testCostoPromedio() {
+    }
+    
+}
---
+++ src/test/java/com/mavha/cap/java/practica01/logica/CursoLogicImplTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mavha.cap.java.practica01.logica;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mdominguez
+ */
+public class CursoLogicImplTest {
+    
+    public CursoLogicImplTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of inscribirAlumno method, of class CursoLogicImpl.
+     */
+    @Test
+    public void testInscribirAlumno() {
+    }
+
+    /**
+     * Test of aprobarAlumno method, of class CursoLogicImpl.
+     */
+    @Test
+    public void testAprobarAlumno() {
+    }
+    
+}
